f6fe8b8704dfe38254aa99b8dd2ae4d7223ee115
CHENG-KAI <top@topde-mbp.attlocal.net>
2017-02-02 15:19:29 -0800

    2/2

--- Best_Time_to_Buy_and_Sell_Stock.py
+++ Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,15 +1,18 @@
 class Solution(object):
     def maxProfit(self,price):
         n = len(price)
+        if not price or len(price) < 2:
+            return 0
         min_price = price[0]
         max_profit = 0
-        if not price or n == 1:
-            return 0
         
         for i in range(1,n):
             min_price = min(min_price,price[i])
             max_profit = max(max_profit,price[i]-min_price)
         return max_profit
+
+#price = [0] // if price == None
+#price =[]  //  if not price 
 
 #time capacity is O(n)
 #space capacity is O(1)
