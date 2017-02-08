fda565751c5160abc7fbbe8248984cd7ae04c2f3
CHENG-KAI <top@topde-mbp.attlocal.net>
2017-02-08 12:32:52 -0800

    2/8

--- 74_Search_a_2D_Matrix.py
+++ 74_Search_a_2D_Matrix.py
@@ -47,4 +47,28 @@ class Solution(object):
 
 
 
+class Solution(object):
+    def searchMatrix(self,matrix,target):
+        
+    #searchMatrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+        import bisect 
+        if not matrix:return False
+        m = len(matrix[0])
+        if m == 0:return False
+        n = len(matrix)
+        if n == 0:return False
+        row = [] 
+        for i in xrange(n):
+            row.append(matrix[i][m-1])
+    
+        r = bisect.bisect_left(row,target)
+        if r == n: return False
+        o = bisect.bisect_left(matrix[r],target)
+        if matrix[r][o] == target:
+            return True
+        else:return False
+
+#apply bisect algorithm 
+
+
 
