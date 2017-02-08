78f8397790652d76a87dc20cd638f7c308c9f771
CHENG-KAI <top@topde-mbp.attlocal.net>
2017-02-07 20:39:20 -0800

    2/7 update

---
+++ 74_Search_a_2D_Matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchMatrix(self,matrix,target):
+        
+    #searchMatrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+        if not matrix:
+            return False
+        
+        m = len(matrix[0])
+        n = len(matrix)
+
+        l, r = 0, m-1 
+        while l <= n-1 and r >= 0:
+            if target == matrix[l][r]:
+                return True
+            elif target > matrix[l][r]:
+                l +=1
+            elif target < matrix[l][r]:
+                r -=1
+        return False
+
+
+#Stair search method
+#if the target is greater than top-right element => go down
+#if the target is greater than top-right element => go left
