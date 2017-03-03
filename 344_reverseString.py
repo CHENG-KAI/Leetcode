b2389ffed2747a2a45620b0378f162df84d710a3
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-03 12:23:08 -0800

    3/3

---
+++ 344_reverseString.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join(reversed(s))
+    def reverseString2(self,s):
+        t = ""
+        q = list(s)
+        r = len(q)
+        for i in xrange(r-1,-1,-1):
+            t = t + q[i]
+        return t 
+    def reverseString3(self,s):
+        return s[::-1]
+        
