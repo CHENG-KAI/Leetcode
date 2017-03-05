69308d1b05978920fdf6ccad3fddef849482fb8f
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-04 22:14:22 -0800

    3/4 update

--- 345_Reverse_Vowels_of_a_String.py
+++ 345_Reverse_Vowels_of_a_String.py
@@ -14,3 +14,21 @@ class Solution(object):
             j-=1
         return "".join(s)
 
+
+    def reverseVowels2(self,s):
+        s = list(s)
+        j = len(s)-1
+        vowel = "aeiouAEIOU"
+        i = 0
+        while i < J:
+            if s[i] in vowel and s[j] in vowel:
+                s[i],s[j] = s[j],s[i]
+                i+=1
+                j-=1
+            if s[i] not in vowel:
+                i+=1
+            if s[j] not in vowel:
+                j-=1
+        return "".join(s)
+                
+                
