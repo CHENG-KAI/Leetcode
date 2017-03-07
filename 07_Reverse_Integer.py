8782ff9c71484f9e02a431876281708fe8e22c8d
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-07 15:55:20 -0800

    3/7

---
+++ 07_Reverse_Integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        if x >= 0:
+            x = int(str(x)[::-1])
+        else:
+            x = int('-'+str(x)[:0:-1]) 
+            
+        if 2147483647 > x > -2147483647:
+            return x
+        else:
+            return 0
+
+
+     def reverse_v2(self, x):
+          if x >= 0:
+            x = int(str(x)[::-1])
+          else:
+            x = int('-'+str(x)[:0:-1])
+          return x if 2147483647 > x > -2147483647 else 0
+         
