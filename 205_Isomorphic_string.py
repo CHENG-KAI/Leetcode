2b587812f0c619035c9bd81e8d9e472087308fa3
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-07 22:59:20 -0800

    3/7

---
+++ 205_Isomorphic_string.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return map(s.find,s) == map(t.find,t)
+
+"""
+>> s = "add"
+>> map(s.find,s)
+>>[0,1,1]
+
+>> s = "add"
+>> map(t.find,t)
+>>[0,1,1]
+
+"""
+
+
+
+"""
+hash table solution
+
+"""
+def halfIsom(s, t):
+    res = {}
+    for i in xrange(len(s)):
+        if s[i] not in res:
+            res[s[i]] = t[i]
+        elif res[s[i]] != t[i]:
+            return False
+    return True
+s = "rssas"
+t = "taabl"    
