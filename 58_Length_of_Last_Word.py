5b36dd0b80717f9ae527f62ab9a3d3124210abcb
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-08 19:51:58 -0800

    3/8

---
+++ 58_Length_of_Last_Word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.split()
+        if s == []:
+            return 0
+        a = len(s)-1
+        return len(s[a])
