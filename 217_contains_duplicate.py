0590192defa462bf05247e7452a7a002b5574af1
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-19 22:23:42 -0700

    3/19

---
+++ 217_contains_duplicate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
