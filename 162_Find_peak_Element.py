35f0d8276a4506bc905d14455c56e19917abe4ed
CHENG-KAI <k20052@yahoo.com.tw>
2017-03-22 15:31:30 -0700

    3/22

---
+++ 162_Find_peak_Element.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = [float('-inf')]+nums+[float('-inf')]
+        for i in range(1,len(nums)-1):
+            if nums[i] > nums[i-1] and  nums[i] > nums[i+1]:
+                return i-1
+
+#O(n)
+
+
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = [float('-inf')]+nums+[float('-inf')]
+        l = 1
+        h = len(nums)-1
+        while l < h:
+            mid = (l+h)/2
+            if nums[mid] > nums[mid+1]:
+                h = mid
+            
+            elif nums[mid] < nums[mid+1]:
+                l = mid+1
+        return l-1
+
+#Olog(n)
---
+++ 189_Rotate_Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        r = len(nums)-k
+        nums[:] = nums[r:]+nums[:r]
+
