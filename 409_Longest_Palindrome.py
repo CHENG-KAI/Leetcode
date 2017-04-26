bad3c89320fd13a887d525705d328fa4b7afd833
CHENG-KAI <k20052@yahoo.com.tw>
2017-04-26 11:35:58 -0700

    4/26

---
+++ 409_Longest_Palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        str = collections.Counter(s)
+        res , single = 0, 0
+        for i in str.values():
+            if i % 2 == 0:
+                res += i
+            else:
+                single = 1
+                res += i/2*2
+        return res + single
+
+
