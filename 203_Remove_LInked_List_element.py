479f1b8d121f32bd95007cff8663b36bdabc312c
CHENG-KAI <k20052@yahoo.com.tw>
2017-05-17 18:23:03 -0700

    5/17

--- 203_Remove_LInked_List_element.py
+++ 203_Remove_LInked_List_element.py
@@ -35,3 +35,50 @@ head.next = ListNode(2)
 head.next.next = ListNode(3) 
 head.next.next.next = ListNode(4) 
 print first.removeElements(head,3)
+
+
+
+
+
+
+
+class ListNode:
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        
+        head, head.next = ListNode(0), head
+        p = head
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else: p = p.next
+        return head.next
+            
+        
+            
+                
+
+
+
+
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+first = Solution()
+first.removeElements(head,4)
+
